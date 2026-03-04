d503cbd1581a2717a3b634d6ee7312bdbce0d55c
Pedro Henrique Avila <wanessakcmelo@gmail.com>
2026-03-04 12:57:42 -0300

    feat: registro das rotas  e integração com repository, service e handlers

--- main.go
+++ main.go
@@ -4,24 +4,27 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
-
 	"desafio-todolist-api/database"
+	"desafio-todolist-api/handlers"
 	"desafio-todolist-api/repository"
+	"desafio-todolist-api/services"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-
 	// Conexão com o MongoDB
 	database.ConnectMongo()
 
-	// Selecionar database
+	// Seleção do database
 	db := database.Client.Database("taskdb")
 
-	// Criar repository
+	// Camadas
 	taskRepository := repository.NewTaskRepository(db)
+	taskService := services.NewTaskService(taskRepository)
+	taskHandler := handlers.NewTaskHandler(taskService)
 
-	// Criar router
+	// Router
 	r := mux.NewRouter()
 
 	// Rota de teste
@@ -29,8 +32,13 @@ func main() {
 		w.Write([]byte("API To Do List rodando"))
 	}).Methods("GET")
 
+	// Rotas do CRUD
+	r.HandleFunc("/tasks", taskHandler.CreateTask).Methods("POST")
+	r.HandleFunc("/tasks", taskHandler.ListTasks).Methods("GET")
+	r.HandleFunc("/tasks/{id}", taskHandler.GetTaskByID).Methods("GET")
+	r.HandleFunc("/tasks/{id}", taskHandler.UpdateTask).Methods("PUT")
+	r.HandleFunc("/tasks/{id}", taskHandler.DeleteTask).Methods("DELETE")
+
 	log.Println("Servidor rodando na porta 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
-
-	_ = taskRepository // temporário
 }
